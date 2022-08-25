8817d84857a8404e302fd8ea7f19fb9288675ddc
LetMeFly666 <814114971@qq.com>
2022-08-25 19:09:55 +0800

    residue

--- Codes/Data2IMFs.py
+++ Codes/Data2IMFs.py
@@ -2,7 +2,7 @@
 Author: LetMeFly
 Date: 2022-08-24 20:49:20
 LastEditors: LetMeFly
-LastEditTime: 2022-08-25 16:52:05
+LastEditTime: 2022-08-25 19:09:52
 '''
 import numpy as np
 from PyEMD import EMD  # , Visualisation
@@ -26,14 +26,15 @@ def data2IMFs(data):
     #components = EEMD()(S)
     #imfs, res = components[:-1], components[-1]
 
-    fig, axes = plt.subplots(imfs.shape[0], 1)
+    fig, axes = plt.subplots(imfs.shape[0] + 1, 1)
     if imfs.shape[0] == 1:
         axes = list(axes)
     axes[0].set_title("The IMFs.")
     for num, IMF in enumerate(imfs):
         axes[num].plot(np.arange(0, 40, 0.01), IMF)
         axes[num].set_ylabel("IMF " + str(num + 1))
-
+    axes[imfs.shape[0]].plot(np.arange(0, 40, 0.01), res)
+    axes[imfs.shape[0]].set_ylabel("Residue")
 
     # vis = Visualisation()
     # vis.plot_imfs(imfs=imfs, residue=res, t=t, include_residue=True)
