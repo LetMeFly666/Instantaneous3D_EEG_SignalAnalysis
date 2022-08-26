d05aa443c78b9739fd0317483203b3cdaccfbda4
LetMeFly666 <814114971@qq.com>
2022-08-26 09:35:40 +0800

    使用了封装的函数

--- Codes/IMFs2FrequencyDomain.py
+++ Codes/IMFs2FrequencyDomain.py
@@ -2,19 +2,14 @@
 Author: LetMeFly
 Date: 2022-08-25 16:25:58
 LastEditors: LetMeFly
-LastEditTime: 2022-08-25 19:47:47
+LastEditTime: 2022-08-26 09:34:50
 '''
 import numpy as np
 import matplotlib.pyplot as plt
+from Visualize import showIMFs
 def IMFs2FrequencyDomain(IMFs):
     for i in range(len(IMFs)):
         IMFs[i] = np.fft.fft(IMFs[i])
-    fig, axes = plt.subplots(IMFs.shape[0], 1)
-    if IMFs.shape[0] == 1:
-        axes = list(axes)
-    axes[0].set_title("Change IMFs into frequency domain")
-    for num, IMF in enumerate(IMFs):
-        axes[num].plot(np.arange(0, 40, 0.01), IMF)
-        axes[num].set_ylabel("IMF " + str(num + 1))
-    plt.show()
+    
+    showIMFs(IMFs, 0, 40, "Change IMFs into frequency domain")
     return IMFs
