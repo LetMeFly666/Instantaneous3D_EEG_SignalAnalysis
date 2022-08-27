f58ed3f9298113d33b679b244a86f16e0139e699
LetMeFly666 <814114971@qq.com>
2022-08-27 21:55:33 +0800

    not cut

--- Codes/CutoffNoise.py
+++ Codes/CutoffNoise.py
@@ -2,14 +2,14 @@
 Author: LetMeFly
 Date: 2022-08-25 10:50:54
 LastEditors: LetMeFly
-LastEditTime: 2022-08-27 21:50:36
+LastEditTime: 2022-08-27 21:55:23
 '''
 import numpy as np
 from matplotlib import pyplot as plt
 from Visualize import showIMFs
 
 def cutoffNoise(IMFs):
-    # return IMFs  # Try result
+    return IMFs  # Try result
     IMFs[IMFs < 0.5] = 0
     IMFs[IMFs > 32] = 0
     showIMFs(IMFs, 0, 40, "Cut off noise")
