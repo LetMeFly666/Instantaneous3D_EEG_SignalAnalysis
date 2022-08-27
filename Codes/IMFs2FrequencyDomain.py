55f35c34aa2215f3ecbd77efc1e7e29220861890
LetMeFly666 <814114971@qq.com>
2022-08-27 21:52:44 +0800

    似乎完成了时域到频域的转换

--- Codes/IMFs2FrequencyDomain.py
+++ Codes/IMFs2FrequencyDomain.py
@@ -2,16 +2,30 @@
 Author: LetMeFly
 Date: 2022-08-25 16:25:58
 LastEditors: LetMeFly
-LastEditTime: 2022-08-26 20:02:35
+LastEditTime: 2022-08-27 21:47:41
 '''
+from scipy.fftpack import fft
 import numpy as np
 from Visualize import showIMFs
+
 def IMFs2FrequencyDomain(IMFs):
-    for i in range(len(IMFs)):
-        IMFs[i] = np.fft.fft(IMFs[i])
-    
-    showIMFs(IMFs, 0, 40, "Change IMFs into frequency domain")
+    for num, IMF in enumerate(IMFs):
+        frequency = fft(IMF)
+        magnitude = np.abs(frequency)
+        phase = np.angle(frequency)
+        IMFs[num] = magnitude
+
+    showIMFs(IMFs, 0, 40, "Change IMFs into frequency domain")    
     return IMFs
+
+# import numpy as np
+# from Visualize import showIMFs
+# def IMFs2FrequencyDomain(IMFs):
+#     for i in range(len(IMFs)):
+#         IMFs[i] = np.fft.fft(IMFs[i])
+    
+#     showIMFs(IMFs, 0, 40, "Change IMFs into frequency domain")
+#     return IMFs
 
 
 # from scipy.fftpack import fft
