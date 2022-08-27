3f04ce57a9265f0e28a04bfbff556578e29d7fe0
LetMeFly666 <814114971@qq.com>
2022-08-27 21:52:53 +0800

    cutoff noise

--- Codes/CutoffNoise.py
+++ Codes/CutoffNoise.py
@@ -2,20 +2,15 @@
 Author: LetMeFly
 Date: 2022-08-25 10:50:54
 LastEditors: LetMeFly
-LastEditTime: 2022-08-25 20:13:51
+LastEditTime: 2022-08-27 21:50:36
 '''
 import numpy as np
 from matplotlib import pyplot as plt
+from Visualize import showIMFs
+
 def cutoffNoise(IMFs):
-    return IMFs  # Try result
+    # return IMFs  # Try result
     IMFs[IMFs < 0.5] = 0
     IMFs[IMFs > 32] = 0
-    fig, axes = plt.subplots(IMFs.shape[0], 1)
-    if IMFs.shape[0] == 1:
-        axes = list(axes)
-    axes[0].set_title("Cut off noise")
-    for num, IMF in enumerate(IMFs):
-        axes[num].plot(np.arange(0, 40, 0.01), IMF)
-        axes[num].set_ylabel("IMF " + str(num + 1))
-    plt.show()
+    showIMFs(IMFs, 0, 40, "Cut off noise")
     return IMFs
