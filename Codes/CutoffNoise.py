db3ff28d2ceb332c31013da9b8011bc6df2ed4c7
LetMeFly666 <814114971@qq.com>
2022-08-29 20:49:34 +0800

    按比例移除

--- Codes/CutoffNoise.py
+++ Codes/CutoffNoise.py
@@ -2,7 +2,7 @@
 Author: LetMeFly
 Date: 2022-08-25 10:50:54
 LastEditors: LetMeFly
-LastEditTime: 2022-08-29 20:38:17
+LastEditTime: 2022-08-29 20:48:14
 '''
 import numpy as np
 from matplotlib import pyplot as plt
@@ -11,12 +11,17 @@ from BaseClass import Data
 
 def cutoffNoise(IMFs: Data) -> Data:
     # return IMFs  # Try result
-    originalData = IMFs.data.copy()
+    # originalData = IMFs.data.copy()
+    originalMaxVal = []
+    resizeRate = 35
     for th, IMF in enumerate(IMFs.data):
         maxVal = IMF.max()
-        IMFs.data[th] = IMF * 32 / maxVal
+        originalMaxVal.append(maxVal)
+        IMFs.data[th] = IMF * resizeRate / maxVal
     IMFs.data[IMFs.data < 0.5] = 0
     IMFs.data[IMFs.data > 32] = 0
     showIMFs(IMFs, "Cut off noise")
-    IMFs.data = originalData
+    for th, IMF in enumerate(IMFs.data):
+        IMFs.data[th] = IMF * originalMaxVal[th] / resizeRate
+    # IMFs.data = originalData
     return IMFs
