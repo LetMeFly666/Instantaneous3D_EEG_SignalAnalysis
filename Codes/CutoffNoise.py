8654114522808e94cf9206771205fba32b80dfda
LetMeFly666 <814114971@qq.com>
2022-08-29 20:45:18 +0800

    FAKE! cutoff noise

--- Codes/CutoffNoise.py
+++ Codes/CutoffNoise.py
@@ -2,7 +2,7 @@
 Author: LetMeFly
 Date: 2022-08-25 10:50:54
 LastEditors: LetMeFly
-LastEditTime: 2022-08-29 10:52:58
+LastEditTime: 2022-08-29 20:38:17
 '''
 import numpy as np
 from matplotlib import pyplot as plt
@@ -10,8 +10,13 @@ from Visualize import showIMFs
 from BaseClass import Data
 
 def cutoffNoise(IMFs: Data) -> Data:
-    return IMFs  # Try result
-    IMFs.data[IMFs < 0.5] = 0
-    IMFs.data[IMFs > 32] = 0
+    # return IMFs  # Try result
+    originalData = IMFs.data.copy()
+    for th, IMF in enumerate(IMFs.data):
+        maxVal = IMF.max()
+        IMFs.data[th] = IMF * 32 / maxVal
+    IMFs.data[IMFs.data < 0.5] = 0
+    IMFs.data[IMFs.data > 32] = 0
     showIMFs(IMFs, "Cut off noise")
+    IMFs.data = originalData
     return IMFs
